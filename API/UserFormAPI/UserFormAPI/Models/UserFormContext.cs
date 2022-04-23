f3184ec991f3018e6614130161a8190a62bfcf3e
donaldmema <mema.donald@gmail.com>
2022-04-23 19:21:25 +0300

    Setup Sql database connetion

---
+++ API/UserFormAPI/UserFormAPI/Models/UserFormContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace UserFormAPI.Models
+{
+    public class UserFormContext:DbContext
+    {
+        public UserFormContext(DbContextOptions<UserFormContext> options):base(options)
+        {
+
+        }
+
+        public DbSet<UserForm> Users { get; set; }
+    }
+}
