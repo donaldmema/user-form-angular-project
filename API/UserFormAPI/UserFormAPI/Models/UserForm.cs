c9dba782a1c3b0dedac89c1bd855b0179697c28b
donaldmema <mema.donald@gmail.com>
2022-04-23 18:12:07 +0300

    Create .Net Core API draft

---
+++ API/UserFormAPI/UserFormAPI/Models/UserForm.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UserFormAPI.Models
+{
+    public class UserForm
+    {
+        [Key]
+        public int UserId { get; set; }
+
+        [Column(TypeName ="nvarchar(100)")]
+        public string Name { get; set; }
+
+        [Column(TypeName = "nvarchar(100)")]
+        public string Surname { get; set; }
+
+        [Column(TypeName = "nvarchar(10)")]
+        public string BirthDate { get; set; }
+
+        [Column(TypeName = "nvarchar(100)")]
+        public string Email { get; set; }
+
+        [Column(TypeName = "nvarchar(15)")]
+        public string Phone { get; set; }
+
+        [Column(TypeName = "nvarchar(100)")]
+        public string Password { get; set; }
+    }
+}
