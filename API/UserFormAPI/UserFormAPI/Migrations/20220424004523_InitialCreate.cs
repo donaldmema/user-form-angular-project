79058de714915f251098ca50688b7c6674b42b8d
donaldmema <mema.donald@gmail.com>
2022-04-24 03:47:53 +0300

    Migrate database using EF Core

---
+++ API/UserFormAPI/UserFormAPI/Migrations/20220424004523_InitialCreate.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace UserFormAPI.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    UserId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(100)", nullable: false),
+                    Surname = table.Column<string>(type: "nvarchar(100)", nullable: false),
+                    BirthDate = table.Column<string>(type: "nvarchar(10)", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(100)", nullable: false),
+                    Phone = table.Column<string>(type: "nvarchar(15)", nullable: false),
+                    Password = table.Column<string>(type: "nvarchar(100)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.UserId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
